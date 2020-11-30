0536a980ce8937f0807502f72620da30d46ea9c1
richardcrng@gmail.com <richardcrng@gmail.com>
2020-11-30 21:50:36 +0000

    Link main title to /

--- components/SiteNav/index.tsx
+++ components/SiteNav/index.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 
 const paths: Record<string, string> = {
-  Home: '/',
+  // Home: '/',
   Work: '/work',
   Speaking: '/speaking',
   Writing: '/blog'
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import Link from 'next/link'
 
 import "react-notion/src/styles.css";
 import "prismjs/themes/prism-tomorrow.css";
@@ -9,7 +10,7 @@ import SiteNav from '../components/SiteNav';
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='content'>
-      <h1>Richard Ng</h1>
+      <h1><Link href='/'>Richard Ng</Link></h1>
       <SiteNav />
       <br />
       <Component {...pageProps} />
