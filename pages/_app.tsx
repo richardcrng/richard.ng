18e40ebb8eb4238156bb84d74550a6046282705b
richardcrng@gmail.com <richardcrng@gmail.com>
2020-11-30 23:10:17 +0000

    Add calendar CTA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,7 +13,32 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className='content'>
       <h1><Link href='/'>Richard Ng</Link></h1>
-      <p><b>Tech, education, startups, D&I</b></p>
+      <div style={{
+        display: 'flex',
+        width: '100%',
+        justifyContent: 'space-between',
+        // marginBottom: '1rem'
+      }}>
+        <span><b>Tech, education, startups, D&I</b></span>
+        <button><a href='https://calendly.com/richard-ng/serendipity-slots' target='_blank'>Calendar me</a></button>
+        <style jsx>{`
+          button {
+            background-color: white;
+            outline-color: black;
+            transition-duration: 0.4s;
+          }
+
+          button a {
+            text-decoration: none;
+          }
+
+          button:hover {
+            background-color: black;
+            outline-color: white;
+            color: white;
+          }
+        `}</style>
+      </div>
       <div style={{
         display: 'flex',
         width: '100%',
