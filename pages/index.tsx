54e2ccce252d15645e7731cc5108f01c824183a1
richardcrng@gmail.com <richardcrng@gmail.com>
2020-12-05 11:38:50 +0000

    Add site title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-const NOTION_PAGE_ID = "7378f66a7b2f4cb19cd101b2f7a496ec";
-
 import { NotionRenderer, BlockMapType } from "react-notion";
+
+const NOTION_PAGE_ID = "7378f66a7b2f4cb19cd101b2f7a496ec";
 
 export async function getStaticProps() {
   const blocks = await fetch(
