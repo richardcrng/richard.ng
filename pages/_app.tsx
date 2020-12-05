683493d9fd65f6c31f2b5e28973aefe11782fe2f
richardcrng@gmail.com <richardcrng@gmail.com>
2020-12-05 11:35:50 +0000

    Reorganise styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import { AppProps } from "next/app";
-import Head from 'next/head';
 import Link from 'next/link'
 
 import "react-notion/src/styles.css";
@@ -8,60 +7,27 @@ import "prismjs/themes/prism-tomorrow.css";
 import "../styles.css";
 import SiteNav from '../components/SiteNav';
 import Socials from "../components/Socials";
+import Metadata from "../components/Metadata";
 
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff" />
-      </Head>
+      <Metadata />
       <div className='content'>
         <h1><Link href='/'>Richard Ng</Link></h1>
-        <div style={{
-          display: 'flex',
-          width: '100%',
-          justifyContent: 'space-between',
-          // marginBottom: '1rem'
-        }}>
+        <div className='introduction'>
           <span><b>Tech, education, startups, D&I</b></span>
-          <button><a href='https://calendly.com/richard-ng/serendipity-slots' target='_blank'>Calendar me</a></button>
-          <style jsx>{`
-            button {
-              background-color: white;
-              outline-color: black;
-              transition-duration: 0.4s;
-            }
-
-            button a {
-              text-decoration: none;
-            }
-
-            button:hover {
-              background-color: black;
-              outline-color: white;
-              color: white;
-            }
-          `}</style>
+          <button className='main-cta'>
+            <a href='https://calendly.com/richard-ng/serendipity-slots' target='_blank'>Calendar me</a>
+          </button>
         </div>
-        <div style={{
-          display: 'flex',
-          width: '100%',
-          justifyContent: 'space-between'
-        }}>
+        <div className='navbar'>
           <SiteNav />
           <Socials />
         </div>
         <br />
-        <div style={{
-          marginBottom: '2rem'
-        }}>
+        <div className='main-body'>
           <Component {...pageProps} />
         </div>
       </div>
