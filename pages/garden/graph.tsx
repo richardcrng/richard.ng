d8a2c5634c8834d7c82f6fe26b283fd044fcf0d9
Richard Ng <richardcrng@gmail.com>
2020-12-31 18:23:55 +0000

    Pass static props to garden graph

--- pages/garden/graph.tsx
+++ pages/garden/graph.tsx
@@ -1,40 +1,45 @@
+import { GetStaticPropsResult } from "next";
 import dynamic from "next/dynamic";
+import { GraphData } from "react-force-graph-2d";
 import Page from "../../components/Page";
 
 const ForceGraph2D = dynamic(() => import("react-force-graph-2d"), {
   ssr: false,
 });
-// const ForceGraph3D = dynamic(() => import("react-force-graph-3d"), {
-//   ssr: false,
-// });
 
-function GardenGraphPage() {
+export async function getStaticProps(): Promise<
+  GetStaticPropsResult<{ graphData: GraphData }>
+> {
+  return {
+    props: {
+      graphData: {
+        nodes: [
+          {
+            id: "id1",
+            // @ts-ignore
+            name: "name1",
+          },
+          {
+            id: "id2",
+            // @ts-ignore
+            name: "name2",
+          },
+        ],
+        links: [
+          {
+            source: "id1",
+            target: "id2",
+          },
+        ],
+      },
+    },
+  };
+}
+
+function GardenGraphPage({ graphData }: { graphData: GraphData }) {
   return (
     <Page title="Garden Graph">
-      <ForceGraph2D
-        graphData={{
-          nodes: [
-            {
-              id: "id1",
-              // @ts-ignore
-              name: "name1",
-              val: 1,
-            },
-            {
-              id: "id2",
-              // @ts-ignore
-              name: "name2",
-              val: 10,
-            },
-          ],
-          links: [
-            {
-              source: "id1",
-              target: "id2",
-            },
-          ],
-        }}
-      />
+      <ForceGraph2D graphData={graphData} />
     </Page>
   );
 }
