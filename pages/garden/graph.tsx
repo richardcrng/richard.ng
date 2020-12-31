1e354cad22129f9e142eb8de037e2d8a01707ee8
Richard Ng <richardcrng@gmail.com>
2020-12-31 18:13:09 +0000

    Dynamic import of force graphs

---
+++ pages/garden/graph.tsx
@@ -0,0 +1,42 @@
+import dynamic from "next/dynamic";
+import Page from "../../components/Page";
+
+const ForceGraph2D = dynamic(() => import("react-force-graph-2d"), {
+  ssr: false,
+});
+const ForceGraph3D = dynamic(() => import("react-force-graph-3d"), {
+  ssr: false,
+});
+
+function GardenGraphPage() {
+  return (
+    <Page title="Garden Graph">
+      <ForceGraph2D
+        graphData={{
+          nodes: [
+            {
+              id: "id1",
+              // @ts-ignore
+              name: "name1",
+              val: 1,
+            },
+            {
+              id: "id2",
+              // @ts-ignore
+              name: "name2",
+              val: 10,
+            },
+          ],
+          links: [
+            {
+              source: "id1",
+              target: "id2",
+            },
+          ],
+        }}
+      />
+    </Page>
+  );
+}
+
+export default GardenGraphPage;
