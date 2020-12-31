d85c9fe9851a7f3bc52d1efab1caea341bfb38e9
Richard Ng <richardcrng@gmail.com>
2020-12-31 18:28:30 +0000

    Generate graph from internal links

--- pages/garden/graph.tsx
+++ pages/garden/graph.tsx
@@ -1,7 +1,8 @@
 import { GetStaticPropsResult } from "next";
 import dynamic from "next/dynamic";
-import { GraphData } from "react-force-graph-2d";
+import { GraphData, LinkObject, NodeObject } from "react-force-graph-2d";
 import Page from "../../components/Page";
+import { getPublicObisidanNotes } from "../../lib/obsidian";
 
 const ForceGraph2D = dynamic(() => import("react-force-graph-2d"), {
   ssr: false,
@@ -10,27 +11,30 @@ const ForceGraph2D = dynamic(() => import("react-force-graph-2d"), {
 export async function getStaticProps(): Promise<
   GetStaticPropsResult<{ graphData: GraphData }>
 > {
+  const publicNotes = getPublicObisidanNotes();
+  const notesArr = Object.values(publicNotes);
+
+  const nodes: NodeObject[] = notesArr.map((note) => ({
+    id: note.fileName,
+    name: note.fileName,
+  }));
+
+  const links: LinkObject[] = notesArr.reduce((acc, note) => {
+    const linksFromNote = Object.values(note.internalLinks);
+    return [
+      ...acc,
+      ...linksFromNote.map((internalLink) => ({
+        source: note.fileName,
+        target: internalLink.fileName,
+      })),
+    ];
+  }, [] as LinkObject[]);
+
   return {
     props: {
       graphData: {
-        nodes: [
-          {
-            id: "id1",
-            // @ts-ignore
-            name: "name1",
-          },
-          {
-            id: "id2",
-            // @ts-ignore
-            name: "name2",
-          },
-        ],
-        links: [
-          {
-            source: "id1",
-            target: "id2",
-          },
-        ],
+        nodes,
+        links,
       },
     },
   };
