f3690783f08639d7d82e6e2998d3b46ef58af381
Richard Ng <richardcrng@gmail.com>
2021-01-01 09:04:57 +0000

    Create contribution heatmap

--- components/GardenNote/index.tsx
+++ components/GardenNote/index.tsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import { WikiLinkNode, wikiLinkPlugin } from "remark-wiki-link";
 import { AsyncReturnType } from "type-fest";
+import CalendarHeatmap from "react-calendar-heatmap";
+import "react-calendar-heatmap/dist/styles.css";
 import { getCommitDatesForGardenNote } from "../../lib/api/github";
 import {
   ObsidianNoteBase,
@@ -16,6 +19,33 @@ export interface GardenNoteProps {
   commitData: AsyncReturnType<typeof getCommitDatesForGardenNote>;
 }
 
+interface CalendarPoint {
+  date: string;
+  count: number;
+}
+
+const commitDataToCount = (
+  commitData: GardenNoteProps["commitData"]
+): CalendarPoint[] => {
+  const dateDict = commitData.reduce((acc, commitDatum) => {
+    const commitDate = commitDatum.date;
+    if (typeof commitDate === "string") {
+      if (acc[commitDate]) {
+        acc[commitDate].count += 1;
+        return acc;
+      } else {
+        return {
+          ...acc,
+          [commitDate]: { date: commitDate, count: 1 },
+        };
+      }
+    } else {
+      return acc;
+    }
+  }, {} as Record<string, CalendarPoint>);
+  return Object.values(dateDict);
+};
+
 function GardenNote({
   note,
   slugs,
@@ -23,7 +53,18 @@ function GardenNote({
   publicNotes,
   commitData,
 }: GardenNoteProps) {
+  const [currentDate] = useState(new Date());
+  const [dateRangeStart] = useState(() => {
+    const d = new Date();
+    // d.setMonth(d.getMonth() - 3);
+    d.setFullYear(d.getFullYear() - 1);
+    return d;
+  });
+
   if (!note) return null;
+
+  const heatmapData = commitDataToCount(commitData);
+  const largestCount = Math.max(...heatmapData.map((point) => point.count));
 
   const backlinks = Object.values(note.backlinks);
 
@@ -74,7 +115,27 @@ function GardenNote({
 
   return (
     <>
-      {commitData && <pre>{JSON.stringify(commitData, null, 2)}</pre>}
+      {commitData && currentDate && dateRangeStart && (
+        <CalendarHeatmap
+          startDate={dateRangeStart}
+          endDate={currentDate}
+          values={commitDataToCount(commitData)}
+          // classForValue={(value) => {
+          //   // console.log(value, largestCount);
+          //   // if (value >= 0.8 * largestCount) {
+          //   //   return `color-scale-4`;
+          //   // } else if (value >= 0.6 * largestCount) {
+          //   //   return "color-scale-3";
+          //   // } else if (value >= 0.4 * largestCount) {
+          //   //   return "color-scale-2";
+          //   // } else if (value >= 0.2 * largestCount) {
+          //   //   return "color-scale-1";
+          //   // } else {
+          //   //   return "color-empty";
+          //   // }
+          // }}
+        />
+      )}
       <ReactMarkdown plugins={[wikiLinkPluginDetails]} renderers={renderers}>
         {note.markdownContent}
       </ReactMarkdown>
