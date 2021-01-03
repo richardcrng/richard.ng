42ecb011204f2533b3ab0f7d451ce01b0ca8a0b1
Richard Ng <richardcrng@gmail.com>
2021-01-03 08:45:25 +0000

    Run gtag utilities conditional on window.gtag being present

--- utils/gtag.ts
+++ utils/gtag.ts
@@ -5,9 +5,10 @@ export const GA_TRACKING_ID = process.env.GOOGLE_ANALYTICS_MEASUREMENT_ID;
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url: URL) => {
   // @ts-ignore
-  window.gtag("config", GA_TRACKING_ID, {
-    page_path: url,
-  });
+  window.gtag &&
+    window.gtag("config", GA_TRACKING_ID, {
+      page_path: url,
+    });
 };
 
 type GTagEvent = {
@@ -20,9 +21,10 @@ type GTagEvent = {
 // https://developers.google.com/analytics/devguides/collection/gtagjs/events
 export const event = ({ action, category, label, value }: GTagEvent) => {
   // @ts-ignore
-  window.gtag("event", action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
+  window.gtag &&
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: label,
+      value: value,
+    });
 };
