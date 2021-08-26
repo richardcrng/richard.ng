8aa729de7ca102ecf57cf3993b46ebd46956a9d8
richardcrng@gmail.com <richardcrng@gmail.com>
2021-08-26 12:38:41 +0100

    Add effect to remove .notion-viewport blocking div

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,20 @@ import { useRouter } from "next/dist/client/router";
 
 import "ts-polyfill";
 
-import "react-notion/src/styles.css";
-import "prismjs/themes/prism-tomorrow.css";
+// import "react-notion/src/styles.css";
+// import "prismjs/themes/prism-tomorrow.css";
 
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 
 // used for code syntax highlighting (optional)
 import 'prismjs/themes/prism-tomorrow.css'
+
+// used for collection views (optional)
+import 'rc-dropdown/assets/index.css'
+
+// used for rendering equations (optional)
+import 'katex/dist/katex.min.css'
 
 import "../styles.css";
 import SiteNav from "../components/SiteNav";
@@ -27,6 +33,41 @@ export interface CommonPageProps {
 }
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+
+  // to remove weird blocking notion viewport
+  useEffect(() => {
+    const notionViewport = document.querySelector(".notion-viewport");
+    notionViewport?.remove();
+  })
+
+  // useEffect(() => {
+  //   // Select the node that will be observed for mutations
+  //   const targetNode = document.getElementById("__next");
+
+  //   // Options for the observer (which mutations to observe)
+  //   const config = { attributes: true, childList: true, subtree: true };
+
+
+  //   // Create an observer instance linked to the callback function
+  //   const observer = new MutationObserver(() => {
+  //     const notionViewport = document.querySelector('.notion-viewport')
+  //     notionViewport?.remove()
+  //   });
+
+  //   // Start observing the target node for configured mutations
+  //   observer.observe(targetNode!, config);
+
+  //   // Later, you can stop observing
+  //   return function cleanup() {
+  //     observer.disconnect();
+  //   }
+  // })
+
+  useEffect(() => {
+    const notionViewport = document.getElementById('notion-viewport');
+    notionViewport?.remove();
+  })
+
   const [isDarkMode, setDarkMode] = useState(false);
 
   const handleDarkModeChange = (setToDark: boolean) => {
